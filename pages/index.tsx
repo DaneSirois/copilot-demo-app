79365ffc2247f60ab6a27c1385361cecf3a3bb2d
DaneSirois <dane.sirois@lendesk.com>
2023-11-08 15:01:21 -0800

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Movie lookup</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        
+      </main>
+    </div>
+  );
+}
